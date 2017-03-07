9e388835f49f81b09d9e82ba1471d9b3255b1ec5
Grant Rosario <grantster1124@aol.com>
2017-03-06 20:59:21 -0500

    Added inbound and outbound nodes properties and loop through inbound nodes

--- miniFlow.py
+++ miniFlow.py
@@ -1,3 +1,9 @@
 class Node(object):
-	def __init__(self):
-		# Properties will go here!
+	def __init__(self, inbound_nodes=[]):
+		# Node(s) from which this Node receives values
+		self.inbound_nodes = inbound_nodes
+		# Node(s) to which this node passes values
+		self.outbound_nodes = []
+		# For each inbound Node here, add this Node as an outbound Node to _that_ Node
+		for n in self.inbound_nodes:
+			n.outbound_nodes.append(self)
