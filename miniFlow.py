71e3d800aafb0b533e2d4e8c9c2ed6fe4f4799a3
Grant Rosario <grantster1124@aol.com>
2017-03-08 20:56:03 -0800

    Updated comments; added forward() to input class

--- miniFlow.py
+++ miniFlow.py
@@ -24,3 +24,16 @@ class Input(Node):
 		# An Input node has no inbound nodes,
 		# so no need to pass anything to the Node instantiator.
 		Node.__init__(self)
+
+	# Note: Input node is the only node where the value 
+	# may be passed as an argument to forward()
+	#
+	# All other node implementations should get the value
+	# of the previous node from self.inbound_nodes
+	#
+	# Example:
+	# val0 = self.inbound_nodes[0].value
+	def forward(self, value=None):
+		# Overwrite the value if one is passed in
+		if value is not None:
+			self.value = value
