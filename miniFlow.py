54f7a833c10f9956e5c511328ca0989faae84787
Grant Rosario <grantster1124@aol.com>
2017-03-10 23:02:55 -0500

    Added forward method to Add class

--- miniFlow.py
+++ miniFlow.py
@@ -41,3 +41,8 @@ class Input(Node):
 class Add(Node):
 	def __init__(self, x, y):
 		Node.__init__(self, [x, y])
+
+	def forward(self):
+		"""
+		You'll be writing code here in the next quiz!
+		"""
